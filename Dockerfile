8bac3a761ca15aae878acf4cff92416a38567986
Ivan Pedrazas <ipedrazas@gmail.com>
2016-02-25 16:26:21 +0000

    write and healthcheck added

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,5 @@ WORKDIR /usr/src/app
 RUN  pip3 install --no-cache-dir --upgrade --ignore-installed flask hvac
 
 COPY app.py /usr/src/app
-COPY certs.pem  /usr/src/app/certs.pem
 
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -4,10 +4,12 @@ Little web app to interface with Vault.
 """
 import os
 import hvac
+import ast
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
+
 app = Flask(__name__)
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 
 @app.route("/")
@@ -23,16 +25,54 @@ def hello():
 def read():
     """Read KVs from Vault."""
     key = request.args.get('key')
+    try:
+        resp = read_key(key)
+        return jsonify(resp)
+    except Exception, e:
+        return jsonify(key=str(e))
+
+
+def read_key(key):
+    """Read key from Vault."""
     client = hvac.Client(
         url=os.environ['VAULT_SERVER'],
         token=os.environ['VAULT_TOKEN'],
-        verify=False)
-    try:
-        val = client.read(key, )
-        return jsonify(key=val)
-    except Exception as e:
-        print (e)
-        return jsonify(key=str(e))
+        verify=ast.literal_eval(os.environ['VERIFY']))
+    val = client.read(key)
+    app.logger.debug(val)
+
+    obj = {'key': key, 'val': val}
+    return obj
+
+
+@app.route("/write", methods=['POST'])
+def write():
+    """Write KVs to Vault."""
+    key = request.form['key']
+    pair_key = request.form['pair_key']
+    pair_value = request.form['pair_value']
+    lease = request.form["lease"]
+
+    client = hvac.Client(
+        url=os.environ['VAULT_SERVER'],
+        token=os.environ['VAULT_TOKEN'],
+        verify=ast.literal_eval(os.environ['VERIFY']))
+    obj = {pair_key: pair_value, "lease": lease}
+    client.write(key, **obj)
+    return jsonify(obj)
+
+
+@app.route("/healthcheck")
+def health():
+    """Healthcheck that verifies if the service can talk to Vault."""
+    client = hvac.Client(
+        url=os.environ['VAULT_SERVER'],
+        token=os.environ['VAULT_TOKEN'],
+        verify=ast.literal_eval(os.environ['VERIFY']))
+    if client:
+        return "ok", 200
+    else:
+        abort(409)
 
 
 if __name__ == "__main__":
